88f64c7b5fb2b4036e10050ea6f6870b2ee7c008
Admin <cyrhelpelias24@gmail.com>
2023-02-18 18:09:35 +0800

    Inital My Application Project

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button = (Button)  findViewById(R.id.buttonsecond);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(SecondActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
