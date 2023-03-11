924357490e140031610789779df4e29ebff6cf2f
Admin <cyrhelpelias24@gmail.com>
2023-03-11 16:45:44 +0800

    March 11 FragmentActivity

--- app/src/main/java/com/example/myapplication/MainFragment.java
+++ app/src/main/java/com/example/myapplication/MainFragment.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import androidx.fragment.app.FragmentTransaction;
+
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainFragment extends AppCompatActivity {
+
+    Button FragA, FragB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fragment);
+
+        FragA = (Button) findViewById(R.id.FragA);
+        FragB = (Button) findViewById(R.id.FragB);
+
+        FragA.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentA());
+            }
+        }));
+
+        FragB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
     }
-}
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fa = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fa.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,
+                fragment);
+        fragmentTransaction.commit();
+
+    }
+    }
